b7525f3669d5333c2322536d62d1f5715e3060df
Bilal Amarni <bilal.amarni@gmail.com>
2012-03-04 18:53:53 +0100

    Using the ContainerBuilder instead of a Cache Factory class.

--- DependencyInjection/ParizzCacheExtension.php
+++ DependencyInjection/ParizzCacheExtension.php
@@ -6,6 +6,8 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -26,6 +28,46 @@ class ParizzCacheExtension extends Extension
         $loader->load('cache.xml');
         $loader->load('event_listener.xml');
 
-        $container->setParameter('parizz_cache.factory.options', $config['storage']);
+        foreach ($config['drivers'] as $name => $options) {
+            $cacheDef = $this->getCacheDriverDefinition($options);
+            $container->setDefinition(sprintf('parizz_cache.%s_driver', $name), $cacheDef);
+        }
+    }
+
+    protected function getCacheDriverDefinition(array $config)
+    {
+        switch ($config['type']) {
+            case 'memcache':
+                $memcacheInstance = new Definition('Memcache');
+                $memcacheInstance->addMethodCall('connect', array(
+                    $config['host'], $config['port']
+                ));
+                $cacheDef = new Definition('%parizz_cache.memcache_driver.class%');
+                $cacheDef->addMethodCall('setMemcache', array($memcacheInstance));
+                break;
+            case 'memcached':
+                $cacheDef = new Definition('%parizz_cache.memcached_driver.class%');
+                $cacheDef->addMethodCall('setMemcached', array(
+                    new Definition('Memcached')
+                ));
+                break;
+            case 'filesystem':
+                $cacheDef = new Definition('%parizz_cache.filesystem_driver.class%');
+                $cacheDef->addMethodCall('setPath', array(
+                    $config['path']
+                ));
+                break;
+            case 'win_cache':
+            case 'xcache':
+            case 'zend_data':
+                $cacheDef = new Definition('%parizz_cache.'.$config['type'].'_driver.class%');
+                break;
+            default:
+                throw new \InvalidArgumentException(sprintf('"%s" is an unrecognized cache driver.', $config['type']));
+        }
+
+        $cacheDef->addMethodCall('setNamespace', array($config['namespace']));
+
+        return $cacheDef;
     }
 }
